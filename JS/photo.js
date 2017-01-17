4b8188325da9ada126dd41eaab4091ed5a02dc8d
Kamal Kumar <kamalkumar@Kamals-MacBook-Pro.local>
2017-01-17 11:08:29 -0500

    start of slideshow

--- JS/photo.js
+++ JS/photo.js
@@ -1,38 +1,16 @@
-//Testing console
-// console.log('ice')
+console.log("ice")
 
-$(document).ready(function(){
-  var isWebkit = navigator && navigator.userAgent.match(/webkit/i);
-  var $root = $(isWebkit ? 'body' : 'html');
-  var elements = $('div'), elcount = elements.length;
-  var scrolling = false;
-  // Replacing the CSS attr(... url)
-  elements.css('background-image', function(i){
-    return 'url('+$(this).data('img')+')';
-  });
-  //Add permalinks
-  elements.each(function(i){
-    var $t = $(this);
-    var id = $t.attr('id');
-    if(!id) return;
-    $('<a>').addClass('permalink')
-            .attr('href', '#'+id)
-            .appendTo($t);
-  });
-  
-  $root.keydown(function(e){
-    if(e.keyCode != 37 && e.keyCode != 39) return;
-    var current = scrolling || 0;
-    if(scrolling === false)
-    {
-      var bsT = $root.scrollTop(), t;
-      while(current < elcount && (t = elements.eq(current).offset().top) < bsT)
-        current++;
-    }
-    if(e.keyCode == 37) current--;
-    else if(scrolling !== false || t == bsT) current++;
-    current = (current + elcount) % elcount;
-    $root.stop().animate({scrollTop: elements.eq(current).offset().top}, function(){scrolling = false;});
-    scrolling = current;
-  });
-});
+var $current, $next, $slides = $(".slideshow .slide");
+
+function doSlideShow () {
+  $current = $slides.filter(".slide.current");
+  $next = $current.next(".slide");
+  if ($next.length < 1) {
+    $next = $slides.first();
+  }
+  $slides.removeClass("previous");
+  $current.addClass("previous").removeClass("current");
+  $next.addClass("current");
+  window.setTimeout(doSlideShow, 5000);
+}
+window.setTimeout(doSlideShow, 5000);
