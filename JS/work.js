de894634ed69c3a0261e8764d8f210a7c4550b86
Kamal Kumar <kamalkumar@Kamals-MacBook-Pro.local>
2017-01-24 11:03:05 -0500

    jquery start

--- JS/work.js
+++ JS/work.js
@@ -1,22 +1,26 @@
-console.log("you're in the work page")
+console.log("you're in the work page");
 
+$(document).ready(function(){
 
-$('.filter a').click(function(e) {
-  e.preventDefault();
-  var a = $(this).attr('href');
-  a = a.substr(1);
-  $('.sets a').each(function() {
-    if (!$(this).hasClass(a) && a != 'all')
-      $(this).addClass('hide');
-    else
-      $(this).removeClass('hide');
-  });
+// testing
+// $("[id = education").css("background-color","yellow");
 
-});
+$education = $("[id = education");
+// console.log("just turned education yellow")
+$education.css("background-color", "yellow")
 
-$('.sets a').click(function(e) {
-  e.preventDefault();
-  var $i = $(this);
-  $('.sets a').not($i).toggleClass('pophide');
-  $i.toggleClass('pop');
+$retail = $("[id = retail");
+$retail.css("background-color", "red")
+
+$mixed = $("[id = mixed");
+$mixed.css("background-color", "green")
+
+$health = $("[id = health");
+$health.css("background-color", "blue")
+
+$civic = $("[id = civic");
+$civic.css("background-color", "papayawhip")
+
+$all = $("[id = all");
+$all.css("background-color", "black")
 });
