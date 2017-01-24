1fe0b479a4f36503c65025572740a3c0cadcbdc3
Kamal Kumar <kamalkumar@Kamals-MacBook-Pro.local>
2017-01-24 09:58:32 -0500

    filter bar

--- JS/work.js
+++ JS/work.js
@@ -1,2 +1,22 @@
 console.log("you're in the work page")
 
+
+$('.filter a').click(function(e) {
+  e.preventDefault();
+  var a = $(this).attr('href');
+  a = a.substr(1);
+  $('.sets a').each(function() {
+    if (!$(this).hasClass(a) && a != 'all')
+      $(this).addClass('hide');
+    else
+      $(this).removeClass('hide');
+  });
+
+});
+
+$('.sets a').click(function(e) {
+  e.preventDefault();
+  var $i = $(this);
+  $('.sets a').not($i).toggleClass('pophide');
+  $i.toggleClass('pop');
+});
