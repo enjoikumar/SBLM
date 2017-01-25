2089a3b34ce4d33c5c1a5ece490e39fc9d9cefbf
Kamal Kumar <kamalkumar@Kamals-MacBook-Pro.local>
2017-01-25 11:00:48 -0500

    slideshow working

--- JS/projects/elmhurst.js
+++ JS/projects/elmhurst.js
@@ -1,40 +1,42 @@
 // console.log("project page")
 $(document).ready(function(){
 
-	//keep track of current slide
-	var counter = 0
+	(function(){
 
-	//a collection of all the slides, caching for performance
-	$items = document.querySelectorAll('.diy-slideshow figure'),
+  // to keep track of current slide
+	var counter = 0, 
 
-	//total number of slides
-	numItems = $items.length;
+	// a collection of all of the slides, caching for performance
+  $items = document.querySelectorAll('.diy-slideshow figure'),
 
-	//this function is what cycles the slides,
-	//showing the next or previous slide and hiding all the others
+  // total number of slides
+  numItems = $items.length; 
+
+	// this function is what cycles the slides, showing the next or previous slide and hiding all the others
 	var showCurrent = function(){
 
-		//uses modula operator to get the actual index of the element to show
-		var itemToShow = Math.abs(counter%numItems);
-
-		//remove .show from whichever element currently has it
-		[].forEach.call($items, function(el){
-			el.classList.remove('show');
-		});
-
-		//add .show to the one item that's supposed to have it
-		$item[itemToShow].classList.add('show');
+	// uses remainder (aka modulo) operator to get the actual index of the element to show
+  var itemToShow = Math.abs(counter%numItems);  
+  
+  // remove .show from whichever element currently has it 
+  [].forEach.call( $items, function(el){
+  	el.classList.remove('show');
+  });
+  
+  // add .show to the one item that's supposed to have it
+  $items[itemToShow].classList.add('show');
 	};
 
-	//add click events to the previous and next buttons
-	document.querySelector('.next').addEventListener('click', function(){
-		counter++;
-		showCurrent();
-	}, false);
+	// add click events to prev & next buttons 
+	document.querySelector('.next').addEventListener('click', function() {
+     counter++;
+     showCurrent();
+  }, false);
 
-	document.querySelector('.prev').addEventListener('click', function(){
-		counter--;
-		showCurrent();
-	}, false);
-
+	document.querySelector('.prev').addEventListener('click', function() {
+     counter--;
+     showCurrent();
+  }, false);
+  
+	})();  
 })
