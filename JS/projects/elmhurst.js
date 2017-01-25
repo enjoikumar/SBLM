b8a374c71d0119b8955c81a1870619c7bf753ada
Kamal Kumar <kamalkumar@Kamals-MacBook-Pro.local>
2017-01-25 10:34:55 -0500

    start on selected project page

---
+++ JS/projects/elmhurst.js
@@ -0,0 +1,12 @@
+// console.log("project page")
+$(document).ready(function(){
+
+//keep track of current slide
+	var counter = 0
+
+	$items = document.querySelectorAll('.diy-slideshow figure'),
+	numItems = $items.length;
+
+
+
+})
