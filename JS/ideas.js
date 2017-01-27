f47c36a1192b23ad6ac55579dce74417e0592565
Kamal Kumar <kamalkumar@Kamals-MacBook-Pro.local>
2017-01-27 10:41:29 -0500

    shuffling is happening, but not displaying

--- JS/ideas.js
+++ JS/ideas.js
@@ -33,17 +33,49 @@ $(document).ready(function(){
 	document.getElementById("div8"),
 	document.getElementById("div9")
 	];
+	// console.log(chain)
 
 	var wrapper = document.getElementById("grid");
 
-	chain.sort(function(a,b){
-		return 0.5 - Math.random()
-	})
+	// function shuffle(array) {
+	// var currentIndex = array.length, temporaryValue, randomIndex;
+	// 	while (0 !== currentIndex) {
+	// 	randomIndex = Math.floor(Math.random() * currentIndex);
+	// 	currentIndex -= 1;
+
+	// 	temporaryValue = array[currentIndex];
+	// 	array[currentIndex] = array[randomIndex];
+	// 	array[randomIndex] = temporaryValue;
+	// 	}
+	// 	return array
+	// }
+	// chain = shuffle(chain);
+	// console.log(chain)
+
+
+	// chain.sort(function(a,b){
+	// 	return 0.5 - Math.random()
+	// })
 
 	// function func1() {
  //  console.log("This is the first.");
 	// }
 	// window.onpageload=func1;
+
+	function shuffle(array){
+		var copy = [], n = array.length, i;
+		//while there remain elements to shuffle
+		while (n) {
+			//pick a remaining element
+			i = Math.floor(Math.random() * n--);
+			//and move it to the new array
+			copy.push(array.splice(i,1)[0]);
+		}
+		return  copy
+	}
+
+	chain = shuffle(chain)
+	console.log(chain)
 
 
 	var modalImg = document.getElementById("img01")
@@ -51,17 +83,17 @@ $(document).ready(function(){
 	img1.onclick = function(){
 		modal.style.display = "block";
 		modalImg.src = this.src;
-		// chain.push( chain.shift());
+		chain.push( chain.shift());
 		// console.log("chain got pushed")
-		// for (var i=0, len = chain.length; i < len; i++){
-		// 	console.log("chain got shuffled")
-		// 	var el = chain[i];
-		// 	wrapper.removeChild(el);
-		// 	console.log("trying to remove child")
-		// 	wrapper.appendChild(el)
-		// 	console.log("child appended")
-		// }
-		// console.log(chain)
+		for (var i=0, len = chain.length; i < len; i++){
+			// console.log("chain got shuffled")
+			var el = chain[i];
+			wrapper.removeChild(el);
+			// console.log("trying to remove child")
+			wrapper.appendChild(el)
+			console.log("child appended")
+		}
+		console.log(chain)
 	}
 
 	img2.onclick = function(){
@@ -133,24 +165,42 @@ $(document).ready(function(){
 // });
 // console.log(arr1)
 
-var arr1 = ["dog", "cat", "panda", "tiger","lion"];
 
-function shuffle(array) {
-	var currentIndex = array.length, temporaryValue, randomIndex;
-	while (0 !== currentIndex) {
-		randomIndex = Math.floor(Math.random() * currentIndex);
-		currentIndex -= 1;
 
-		temporaryValue = array[currentIndex];
-		array[currentIndex] = array[randomIndex];
-		array[randomIndex] = temporaryValue;
-	}
-	return array
-}
+// function shuffle(array) {
+// 	var currentIndex = array.length, temporaryValue, randomIndex;
+// 	while (0 !== currentIndex) {
+// 		randomIndex = Math.floor(Math.random() * currentIndex);
+// 		currentIndex -= 1;
 
-arr1 = shuffle(arr1)
-console.log(arr1)
+// 		temporaryValue = array[currentIndex];
+// 		array[currentIndex] = array[randomIndex];
+// 		array[randomIndex] = temporaryValue;
+// 	}
+// 	return array
+// }
 
+// arr1 = shuffle(arr1)
+// console.log(arr1)
+// chain = shuffle(chain)
+// console.log(chain)
+
+// function shuffle(array){
+// 	var copy = [], n = array.length, i;
+// 	//while there remain elements to shuffle
+// 	while (n) {
+// 		//pick a remaining element
+// 		i = Math.floor(Math.random() * n--);
+// 		//and move it to the new array
+// 		copy.push(array.splice(i,1)[0]);
+// 	}
+// 	return  copy
+// }
+
+// var arr1 = ["dog", "cat", "panda", "tiger","lion"];
+
+// arr1 = shuffle(arr1)
+// console.log(arr1)
 
 
 
