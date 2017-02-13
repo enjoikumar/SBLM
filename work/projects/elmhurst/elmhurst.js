9fedc2c9ec92ff20fb8c411885153df92f32c5a9
Kamal Kumar <kamalkumar@Kamals-MacBook-Pro.local>
2017-02-13 10:31:23 -0500

    updated responsiveness for elmhurst

--- work/projects/elmhurst/elmhurst.js
+++ work/projects/elmhurst/elmhurst.js
@@ -17,7 +17,8 @@ $(document).ready(function(){
   // total number of slides
   numItems = $items.length; 
 
-	// this function is what cycles the slides, showing the next or previous slide and hiding all the others
+	// this function is what cycles the slides, 
+  //showing the next or previous slide and hiding all the others
 	var showCurrent = function(){
 
 	// uses remainder (aka modulo) operator to get the actual index of the element to show
@@ -44,8 +45,48 @@ $(document).ready(function(){
      showCurrent();
   }, false);
   
-	})();  
+	})(); 
+
+
+
+
+  var $animation_elements = $('.animation-element');
+  var $window = $(window);
+
+  $window.on('scroll', check_if_in_view);
+  $window.on('scroll resize', check_if_in_view);
+  $window.trigger('scroll');
+
+  function check_if_in_view() {
+    var window_height = $window.height();
+    var window_top_position = $window.scrollTop();
+    var window_bottom_position = (window_top_position + window_height);
+
+    $.each($animation_elements, function(){
+      var $element = $(this);
+      var element_height = $element.outerHeight();
+      var element_top_position = $element.offset().top;
+      var element_bottom_position = (element_top_position + element_height);
+
+      if ((element_bottom_position >= window_top_position) &&
+        (element_top_position <= window_bottom_position)) {
+        $element.addClass('in-view');
+      } else {
+        $element.removeClass('in-view');
+      }
+    })
+  }
 })
+
+
+
+
+
+
+
+
+
+
 
 
 // 
