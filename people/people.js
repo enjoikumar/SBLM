eede900921a5dda8f26e1dbb79ce9d6f0a8fbc24
Kamal Kumar <kamalkumar@Kamals-MacBook-Pro.local>
2017-03-16 14:08:10 -0400

    people page with slideshow

--- people/people.js
+++ people/people.js
@@ -85,6 +85,22 @@ $(document).ready(function(){
 	})
 })
 
+var $current, $next, $slides = $(".slideshow .slide");
+
+// basically making the slideshow
+function doSlideShow () {
+  $current = $slides.filter(".slide.current"); 
+  $next = $current.next(".slide");
+  if ($next.length < 1) {
+    $next = $slides.first();
+  }
+  $slides.removeClass("previous");
+  $current.addClass("previous").removeClass("current");
+  $next.addClass("current");
+  window.setTimeout(doSlideShow, 4000);
+}
+window.setTimeout(doSlideShow, 4000);
+
 
 
 
