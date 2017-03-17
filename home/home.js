bd02855b1635e11bd67f0fb6cadd19e21bb4f10d
Kamal Kumar <kamalkumar@Kamals-MacBook-Pro.local>
2017-03-17 15:57:36 -0400

    alot of changes, better nav bar

--- home/home.js
+++ home/home.js
@@ -1,10 +1,26 @@
 
 //function enables an extra element to the nav bar
-function myFunction() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-      x.className += " responsive";
-  } else {
-      x.className = "topnav";
-  }
+// function myFunction() {
+//   var x = document.getElementById("myTopnav");
+//   if (x.className === "topnav") {
+//       x.className += " responsive";
+//   } else {
+//       x.className = "topnav";
+//   }
+// }
+
+
+var myNav = document.getElementById("myNav");
+
+var count = 0
+function clicker(){
+	count++;
+	if(count%2){
+  	myNav.style.height = "100%";
+	} else{
+  	myNav.style.height = "0%";
+	}
 }
+
+
+
