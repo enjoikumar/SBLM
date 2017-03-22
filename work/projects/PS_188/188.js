a51e6486f1c3174155052afd19701b4d614cffbb
Kamal Kumar <kamalkumar@tomlopessurface.sblm.local>
2017-03-22 14:00:08 -0400

    ps 188

--- work/projects/PS_188/188.js
+++ work/projects/PS_188/188.js
@@ -3,6 +3,12 @@
 
 $(document).ready(function(){
 
+  $(".carousel").flickity({
+    "wrapAround": true, 
+    "pageDots": false, 
+    "autoPlay": true
+  });
+
   //just another way of writing document.ready
   //but for some reason, need to have both
   //could be a conflict of libraries
@@ -45,6 +51,31 @@ $(document).ready(function(){
   }, false);
   
 	})();  
+
+  var $animation_elements = $('.animation-element');
+  var $window = $(window);
+
+  function check_if_in_view() {
+    var window_height = $window.height();
+    var window_top_position = $window.scrollTop();
+    var window_bottom_position = (window_top_position + window_height);
+   
+    $.each($animation_elements, function() {
+      var $element = $(this);
+      var element_height = $element.outerHeight();
+      var element_top_position = $element.offset().top;
+      var element_bottom_position = (element_top_position + element_height);
+   
+      //check to see if this current container is within viewport
+      if ((element_bottom_position >= window_top_position) &&
+          (element_top_position <= window_bottom_position)) {
+        $element.addClass('in-view');
+      }
+    });
+  }
+
+  $window.on('scroll resize', check_if_in_view);
+  $window.trigger('scroll');
 })
 
 
