ff197e7596ec65d2ffd6a95a66f5db98ade5574a
Kamal Kumar <kamalkumar@tomlopessurface.sblm.local>
2017-03-23 15:05:57 -0400

    burger turns to an x

--- home/home.js
+++ home/home.js
@@ -9,18 +9,35 @@
 //   }
 // }
 
-var myNav = document.getElementById("myNav");
+// var myNav = document.getElementById("myNav");
 
-html = "stuff"
+// var count = 0
+// function clicker(){
+// 	count++;
+// 	if(count%2){
+//   	myNav.style.height = "100%";
+// 	} else{
+//   	myNav.style.height = "0%";
+// 	}
+// }
 
-var count = 0
-function clicker(){
-	count++;
-	if(count%2){
-  	myNav.style.height = "100%";
-	} else{
-  	myNav.style.height = "0%";
-	}
-}
+$(document).ready(function(){
 
+	$myNav = $("[id = myNav]")
+
+	$burger = $('span.burger')
+
+	var count = 0
+
+	$burger.click(function(){
+		count++;
+		if(count%2){
+			$myNav.css("height", "100%")
+			$burger.html('&#9747;')
+		} else{
+			$myNav.css("height", "0%")
+			$burger.html('&#9776;')
+		}
+	})
+})
 
