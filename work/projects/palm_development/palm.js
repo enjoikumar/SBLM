2dc2988affa9f734d2d748a279d4f1dccf47245b
Kamal Kumar <kamalkumar@Kamals-MacBook-Pro.local>
2017-03-30 15:22:14 -0400

    peconic bay

--- work/projects/palm_development/palm.js
+++ work/projects/palm_development/palm.js
@@ -3,54 +3,48 @@
 
 $(document).ready(function(){
 
-  $(".carousel").flickity({
-    "wrapAround": true, 
-    "pageDots": false, 
-    "autoPlay": true
-  });
+ //  //just another way of writing document.ready
+ //  //but for some reason, need to have both
+ //  //could be a conflict of libraries
+	// (function(){
 
-  //just another way of writing document.ready
-  //but for some reason, need to have both
-  //could be a conflict of libraries
-	(function(){
+ //  // to keep track of current slide
+	// var counter = 0, 
 
-  // to keep track of current slide
-	var counter = 0, 
+	// // a collection of all of the slides, caching for performance
+ //  $items = document.querySelectorAll('.slideshow figure'),
 
-	// a collection of all of the slides, caching for performance
-  $items = document.querySelectorAll('.slideshow figure'),
+ //  // total number of slides
+ //  numItems = $items.length; 
 
-  // total number of slides
-  numItems = $items.length; 
+	// // this function is what cycles the slides, showing the next or previous slide and hiding all the others
+	// var showCurrent = function(){
 
-	// this function is what cycles the slides, showing the next or previous slide and hiding all the others
-	var showCurrent = function(){
-
-	// uses remainder (aka modulo) operator to get the actual index of the element to show
-  var itemToShow = Math.abs(counter%numItems);  
+	// // uses remainder (aka modulo) operator to get the actual index of the element to show
+ //  var itemToShow = Math.abs(counter%numItems);  
   
-  // remove .show from whichever element currently has it 
-  [].forEach.call( $items, function(el){
-  	el.classList.remove('show');
-  });
+ //  // remove .show from whichever element currently has it 
+ //  [].forEach.call( $items, function(el){
+ //  	el.classList.remove('show');
+ //  });
   
-  // add .show to the one item that's supposed to have it
-  $items[itemToShow].classList.add('show');
-	};
+ //  // add .show to the one item that's supposed to have it
+ //  $items[itemToShow].classList.add('show');
+	// };
 
-	// add click event to next button, as well as adding 1 to the counter 
-	document.querySelector('.next').addEventListener('click', function() {
-     counter++;
-     showCurrent();
-  }, false);
+	// // add click event to next button, as well as adding 1 to the counter 
+	// document.querySelector('.next').addEventListener('click', function() {
+ //     counter++;
+ //     showCurrent();
+ //  }, false);
 
-  // click event to prev button and subtracting 1 to the counter 
-	document.querySelector('.prev').addEventListener('click', function() {
-     counter--;
-     showCurrent();
-  }, false);
+ //  // click event to prev button and subtracting 1 to the counter 
+	// document.querySelector('.prev').addEventListener('click', function() {
+ //     counter--;
+ //     showCurrent();
+ //  }, false);
   
-	})();  
+	// })();  
 })
 
 
