fc7ad61e4b28f9a40fe3a2cf6b731ab2a15fcf7a
Kamal Kumar <kamalkumar@Kamals-MacBook-Pro.local>
2017-04-11 11:59:54 -0400

    modern fold styles

--- work/projects/modern/modern.js
+++ work/projects/modern/modern.js
@@ -84,6 +84,30 @@ $(document).ready(function(){
         scrollTop: $(".history2").offset().top
     }, 500);
   }); 
+
+  $history = $('.history3');
+  $expand = $('.expand');
+
+  var count = 0;
+
+  //user clicks on plus it turns to minus
+  //it also
+  $expand.click(function(){
+    count++;
+    if(count%2){
+      $history.css("display", "block")
+      $expand.html('&#x2501;')
+      $('html, body').animate({
+        scrollTop: $history.offset().top
+    }, 500);
+    } else{
+      $history.css("display", "none")
+      $expand.html('&#x254B;')
+      $('html, body').animate({
+        scrollTop: $(".history2").offset().top
+    }, 400);
+    }
+  })
 })
 
 
