d193648afaf270b5e479ca2731a6ef47296195d8
Kamal Kumar <kamalkumar@Kamals-MacBook-Pro.local>
2017-04-11 11:04:08 -0400

    dsny and elmhurst edited

--- work/projects/fire/fire.js
+++ work/projects/fire/fire.js
@@ -1,7 +1,7 @@
 
 $(document).ready(function(){
  
-  $down = $('.down')
+  $down = $('.slideshow')
 
 
   $down.click(function() {
--- work/projects/nyc_DOS/dos.js
+++ work/projects/nyc_DOS/dos.js
@@ -59,6 +59,31 @@ $(document).ready(function(){
         scrollTop: $(".history5").offset().top
     }, 500);
   });
+
+
+  $history = $('.history2');
+  $expand = $('.expand');
+
+  var count = 0;
+
+  //user clicks on plus it turns to minus
+  //it also
+  $expand.click(function(){
+    count++;
+    if(count%2){
+      $history.css("display", "block")
+      $expand.html('&#x2501;')
+      $('html, body').animate({
+        scrollTop: $history.offset().top
+    }, 500);
+    } else{
+      $history.css("display", "none")
+      $expand.html('&#x254B;')
+      $('html, body').animate({
+        scrollTop: $(".history5").offset().top
+    }, 400);
+    }
+  })
 })
 
 
