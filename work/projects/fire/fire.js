add754e0d9b919f82513fb7817390aaae6e26098
Kamal Kumar <kamalkumar@Kamals-MacBook-Pro.local>
2017-04-11 10:45:58 -0400

    fire department with text

--- work/projects/fire/fire.js
+++ work/projects/fire/fire.js
@@ -1,13 +1,39 @@
 
 $(document).ready(function(){
  
-   $down = $('.down')
+  $down = $('.down')
+
 
   $down.click(function() {
     $('html, body').animate({
         scrollTop: $(".history1").offset().top
     }, 500);
   });
+
+
+  $history = $('.history2');
+  $expand = $('.expand');
+
+  var count = 0;
+
+  //user clicks on plus it turns to minus
+  //it also
+  $expand.click(function(){
+    count++;
+    if(count%2){
+      $history.css("display", "block")
+      $expand.html('&#x2501;')
+      $('html, body').animate({
+        scrollTop: $history.offset().top
+    }, 500);
+    } else{
+      $history.css("display", "none")
+      $expand.html('&#x254B;')
+      $('html, body').animate({
+        scrollTop: $(".history1").offset().top
+    }, 400);
+    }
+  })
 })
 
 
