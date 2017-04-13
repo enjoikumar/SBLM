735710501226f948155c58437d33fcb03cd6a8f2
Kamal Kumar <kamalkumar@Kamals-MacBook-Pro.local>
2017-04-13 16:20:52 -0400

    ps 188 done

--- work/projects/PS_188/188.js
+++ work/projects/PS_188/188.js
@@ -71,23 +71,6 @@ $(document).ready(function(){
   $window.trigger('scroll');
 
 
-  $history = $(".history2")
-
-  $expand = $('.expand')
-
-  var count = 0
-
-  $expand.click(function(){
-    count++;
-    if(count%2){
-      $history.css("display", "block")
-      $expand.html('&#x2501;')
-    } else{
-      $history.css("display", "none")
-      $expand.html('&#x254B;')
-    }
-  })
-
   $down = $('.down')
 
   $down.click(function() {
@@ -95,6 +78,31 @@ $(document).ready(function(){
         scrollTop: $(".history1").offset().top
     }, 500);
   }); 
+
+
+  $history = $('.history2');
+  $expand = $('.expand');
+
+  var count = 0;
+
+  //user clicks on plus it turns to minus
+  //it also
+  $expand.click(function(){
+    count++;
+    if(count%2){
+      $history.css("display", "block")
+      $expand.html('&#x2501;')
+      $('html, body').animate({
+        scrollTop: $history.offset().top
+    }, 500);
+    } else{
+      $history.css("display", "none")
+      $expand.html('&#x254B;')
+      $('html, body').animate({
+        scrollTop: $(".history1").offset().top
+    }, 400);
+    }
+  })
 })
 
 
