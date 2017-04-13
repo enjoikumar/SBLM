d7d02d19400a4860280ab818c1b09f74818e920f
Kamal Kumar <kamalkumar@Kamals-MacBook-Pro.local>
2017-04-13 12:37:02 -0400

    jardim expand and fold

--- work/projects/jardim/jardim.js
+++ work/projects/jardim/jardim.js
@@ -76,6 +76,24 @@ $(document).ready(function(){
         scrollTop: $(".history6").offset().top
     }, 500);
   }); 
+
+  $fold = $('.fold');
+  $expand = $('.expand');
+
+  var count = 0;
+
+  //user clicks on plus it turns to minus
+  //it also
+  $expand.click(function(){
+    count++;
+    if(count%2){
+      $fold.css("display", "block")
+      $expand.html('&#x2501;')
+    } else{
+      $fold.css("display", "none")
+      $expand.html('&#x254B;')
+    }
+  })
 })
 
 
