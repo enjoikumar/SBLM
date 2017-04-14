15a6d27d0fb2536c6936d53193e7d0cf1c94c5d1
Kamal Kumar <kamalkumar@Kamals-MacBook-Pro.local>
2017-04-14 10:56:39 -0400

    sunny isles

--- work/projects/sunny_isles/sunny.js
+++ work/projects/sunny_isles/sunny.js
@@ -70,6 +70,34 @@ $(document).ready(function(){
 
   $window.on('scroll resize', check_if_in_view);
   $window.trigger('scroll'); 
+
+
+  $down = $('.down')
+
+  $down.click(function() {
+    $('html, body').animate({
+        scrollTop: $(".history5").offset().top
+    }, 500);
+  });
+
+
+  $fold = $('.fold');
+  $expand = $('.expand');
+
+  var count = 0;
+
+  //user clicks on plus it turns to minus
+  //it also
+  $expand.click(function(){
+    count++;
+    if(count%2){
+      $fold.css("display", "block")
+      $expand.html('&#x2501;')
+    } else{
+      $fold.css("display", "none")
+      $expand.html('&#x254B;')
+    }
+  })
 })
 
 
