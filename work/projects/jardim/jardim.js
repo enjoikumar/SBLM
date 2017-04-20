00e68b993d3d0ef1e90ecb1b5aa0ba86ff49ca45
Kamal Kumar <kamalkumar@Kamals-MacBook-Pro.local>
2017-04-20 16:58:52 -0400

    deletions of commented code from jardim down

--- work/projects/jardim/jardim.js
+++ work/projects/jardim/jardim.js
@@ -1,42 +1,28 @@
 
 $(document).ready(function(){
 
-  //just another way of writing document.ready
-  //but for some reason, need to have both
-  //could be a conflict of libraries
 	(function(){
 
-  // to keep track of current slide
 	var counter = 0, 
-
-	// a collection of all of the slides, caching for performance
   $items = document.querySelectorAll('.slideshow figure'),
-
-  // total number of slides
   numItems = $items.length; 
 
-	// this function is what cycles the slides, showing the next or previous slide and hiding all the others
 	var showCurrent = function(){
 
-	// uses remainder (aka modulo) operator to get the actual index of the element to show
   var itemToShow = Math.abs(counter%numItems);  
-  
-  // remove .show from whichever element currently has it 
+
   [].forEach.call( $items, function(el){
   	el.classList.remove('show');
   });
   
-  // add .show to the one item that's supposed to have it
   $items[itemToShow].classList.add('show');
 	};
 
-	// add click event to next button, as well as adding 1 to the counter 
 	document.querySelector('.next').addEventListener('click', function() {
      counter++;
      showCurrent();
   }, false);
 
-  // click event to prev button and subtracting 1 to the counter 
 	document.querySelector('.prev').addEventListener('click', function() {
      counter--;
      showCurrent();
@@ -58,7 +44,6 @@ $(document).ready(function(){
       var element_top_position = $element.offset().top;
       var element_bottom_position = (element_top_position + element_height);
    
-      //check to see if this current container is within viewport
       if ((element_bottom_position >= window_top_position) &&
           (element_top_position <= window_bottom_position)) {
         $element.addClass('in-view');
@@ -82,8 +67,6 @@ $(document).ready(function(){
 
   var count = 0;
 
-  //user clicks on plus it turns to minus
-  //it also
   $expand.click(function(){
     count++;
     if(count%2){
