051b735b7c0fff6280ffc34272adf91d1c68c04e
Kamal Kumar <kamalkumar@Kamals-MacBook-Pro.local>
2017-04-26 15:02:19 -0400

    updated site map

--- work/projects/throgs_neck/throgs.js
+++ work/projects/throgs_neck/throgs.js
@@ -34,27 +34,28 @@ $(document).ready(function(){
   
   })();  
 
-  var $animation_elements = $('.animation-element');
-  var $window = $(window);
+  $down = $('.down')
 
-  function check_if_in_view() {
-    var window_height = $window.height();
-    var window_top_position = $window.scrollTop();
-    var window_bottom_position = (window_top_position + window_height);
-   
-    $.each($animation_elements, function() {
-      var $element = $(this);
-      var element_height = $element.outerHeight();
-      var element_top_position = $element.offset().top;
-      var element_bottom_position = (element_top_position + element_height);
-   
-      if ((element_bottom_position >= window_top_position) &&
-          (element_top_position <= window_bottom_position)) {
-        $element.addClass('in-view');
-      }
-    });
-  }
+  $down.click(function() {
+    $('html, body').animate({
+        scrollTop: $(".history1").offset().top
+    }, 500);
+  });
+  
+  $fold = $('.fold');
+  $expand = $('.expand');
 
-  $window.on('scroll resize', check_if_in_view);
-  $window.trigger('scroll');
+  var count = 0;
+
+  $expand.click(function(){
+    count++;
+    if(count%2){
+      $fold.css("display", "block")
+      $expand.html('&#x2501;')
+      $fold.removeClass('away')
+    } else{
+      $expand.html('&#x254B;')
+      $fold.addClass('away')
+    }
+  })
 })
