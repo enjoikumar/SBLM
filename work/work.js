1a122ee1ac4f04099ebae775d2146c764bb24ef3
Kamal Kumar <kamalkumar@Kamals-MacBook-Pro.local>
2017-05-01 12:05:09 -0400

    ps 188 mobile responsive

--- work/work.js
+++ work/work.js
@@ -18,7 +18,7 @@ $(document).ready(function(){
 
   var $edu = function(){
     $education.click(function(){
-      $("#education").css("background-color", "white !important");
+      $('#education').css("background-color", "white");
       $("#education").css("color", "black !important");
       $("#retail").css("background-color", "#212121");
       $("#retail").css("color", "white");
