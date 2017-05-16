7641760484202528a58fea16023909a535362501
Kamal Kumar <kamalkumar@Kamals-MacBook-Pro.local>
2017-05-16 15:23:11 -0400

    updated sitemap dimensions

--- work/projects/petco/petco.js
+++ work/projects/petco/petco.js
@@ -67,6 +67,16 @@ $(document).ready(function(){
 
 
 
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+var owl = {
+  title: "Excellent Owl",
+  src: "https://s3.amazonaws.com/codecademy-content/courses/React/react_photo-owl.jpg"
+};
+
+
+
 
 
 
