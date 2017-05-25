2892703833645f2be5b586c906c85e95fdde9157
Kamal Kumar <kamalkumar@Kamals-MacBook-Pro.local>
2017-05-25 11:21:30 -0400

    home page 736 h2

--- work/projects/petco/petco.js
+++ work/projects/petco/petco.js
@@ -67,15 +67,6 @@ $(document).ready(function(){
 
 
 
-var React = require('react');
-var ReactDOM = require('react-dom');
-
-var owl = {
-  title: "Excellent Owl",
-  src: "https://s3.amazonaws.com/codecademy-content/courses/React/react_photo-owl.jpg"
-};
-
-
 
 
 
