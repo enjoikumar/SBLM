a0825f0b64f7810b3e91e2ba6bee0f73c8e6786e
Kamal Kumar <kamalkumar@Kamals-MacBook-Pro.local>
2017-06-06 13:40:09 -0400

    update

--- about/about.js
+++ about/about.js
@@ -1,3 +1,47 @@
 $(document).ready(function(){
-	
+
+	var $mix = $('.mixabout');
+	var $edu = $('.eduabout');
+	var $ret = $('.retabout');
+	var $hea = $('.heaabout');
+	var $civ = $('.civabout');
+
+	var $mixLight = function(){
+		$mix.css("background-color", "rgb(238, 60, 44)");
+	}
+	// $mixLight();
+
+	var $eduLight = function(){
+		$edu.css("background-color", "rgb(238, 60, 44)");
+	}
+	// $eduLight();
+
+	var $retLight = function(){
+		$ret.css("background-color", "rgb(238, 60, 44)");
+	}
+	// $retLight();
+
+	var $heaLight = function(){
+		$hea.css("background-color", "rgb(238, 60, 44)");
+	}
+	// $heaLight();
+
+	var $civLight = function(){
+		$civ.css("background-color", "rgb(238, 60, 44)");
+	}
+	// $civLight();	 
+
+	// var colors = [ $mix, $edu, $ret, $hea, $civ ];
+	// var colorChange = function(){
+	// 	for(i = 0; i < colors.length; i++){
+	// 		colors[i].css("background-color", "rgb(238, 60, 44)");
+	// 	}
+	// }
+	// colorChange();
+	// setInterval(colorChange, 2000)
+
 })
+
+
+
+
