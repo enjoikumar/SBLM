d6403cd6920eccac3bdd87869e6c9e355fc5c966
Kamal Kumar <kamalkumar@Kamals-MacBook-Pro.local>
2017-06-26 10:34:54 -0400

    animation added back, but removed on mobile responsive

--- work/projects/PS_133/133.js
+++ work/projects/PS_133/133.js
@@ -1,38 +1,61 @@
 
 $(document).ready(function(){
 
-	(function(){
+  (function(){
 
-	var counter = 0, 
+  var counter = 0, 
 
   $items = document.querySelectorAll('.slideshow figure'),
 
   numItems = $items.length; 
 
-	var showCurrent = function(){
+  var showCurrent = function(){
 
   var itemToShow = Math.abs(counter%numItems);  
   
   [].forEach.call( $items, function(el){
-  	el.classList.remove('show');
+    el.classList.remove('show');
   });
   
   $items[itemToShow].classList.add('show');
-	};
+  };
 
-	document.querySelector('.next').addEventListener('click', function() {
+  document.querySelector('.next').addEventListener('click', function() {
      counter++;
      showCurrent();
   }, false);
 
-	document.querySelector('.prev').addEventListener('click', function() {
+  document.querySelector('.prev').addEventListener('click', function() {
      counter--;
      showCurrent();
   }, false);
   
-	})();
+  })();
 
-  
+  var $animation_elements = $('.animation-element');
+  var $window = $(window);
+
+  function check_if_in_view() {
+    var window_height = $window.height();
+    var window_top_position = $window.scrollTop();
+    var window_bottom_position = (window_top_position + window_height);
+   
+    $.each($animation_elements, function() {
+      var $element = $(this);
+      var element_height = $element.outerHeight();
+      var element_top_position = $element.offset().top;
+      var element_bottom_position = (element_top_position + element_height);
+   
+      if ((element_bottom_position >= window_top_position) &&
+          (element_top_position <= window_bottom_position)) {
+        $element.addClass('in-view');
+      }
+    });
+  }
+
+  $window.on('scroll resize', check_if_in_view);
+  $window.trigger('scroll');  
+
 
   $down = $('.down')
 
@@ -67,5 +90,7 @@ $(document).ready(function(){
     }, 800)
   })
 
-
+  if($(window).width() <= 800){
+    $('.animation-element').removeClass('animation-element')
+  };
 })
