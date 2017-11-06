0e9c4f68644a73b067ef5b61b6fd2b8a50a2aaf2
Kamal Kumar <kamalkumar@dellt3610-13.sblm.local>
2017-11-06 16:00:49 -0500

    changed mixed use to eventi

---
+++ assets/js/contact.js
@@ -0,0 +1,28 @@
+
+
+var d = new Date();
+
+var month = d.getMonth()+1;
+var day = d.getDate();
+
+var output = (month < 10 ? '0' : '') + 
+	month + '/' +(day<10 ? '0' : '') + 
+	day + '/' + d.getFullYear();
+
+console.log(output)
+
+$(document).ready(function(){
+
+	var $output = output
+
+	console.log($output)
+
+	$posting = $('.posting')
+
+	console.log($posting)
+
+	$posting.append(' Posted on: ' + $output)
+
+})
+
+
