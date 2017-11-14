1061844e85770024c6fcd44c7bb90981bbe566d5
Kamal Kumar <kamalkumar@dellt3610-13.sblm.local>
2017-11-14 10:25:22 -0500

    added photo

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -1,28 +1,28 @@
 
 
-var d = new Date();
+// var d = new Date();
 
-var month = d.getMonth()+1;
-var day = d.getDate();
+// var month = d.getMonth()+1;
+// var day = d.getDate();
 
-var output = (month < 10 ? '0' : '') + 
-	month + '/' +(day<10 ? '0' : '') + 
-	day + '/' + d.getFullYear();
+// var output = (month < 10 ? '0' : '') + 
+// 	month + '/' +(day<10 ? '0' : '') + 
+// 	day + '/' + d.getFullYear();
 
-console.log(output)
+// console.log(output)
 
-$(document).ready(function(){
+// $(document).ready(function(){
 
-	var $output = output
+// 	var $output = output
 
-	console.log($output)
+// 	console.log($output)
 
-	$posting = $('.posting')
+// 	$posting = $('.posting')
 
-	console.log($posting)
+// 	console.log($posting)
 
-	$posting.append(' Posted on: ' + $output)
+// 	$posting.append(' Posted on: ' + $output)
 
-})
+// })
 
 
